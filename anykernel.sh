182aaa481157392c17228cb96a08632cd4c9cb9a
TP4HCEP <123848993+TP4HCEP@users.noreply.github.com>
2023-02-02 05:50:53 +0300

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -38,6 +38,9 @@ ramdisk_compression=auto;
 
 ## AnyKernel boot install
 dump_boot;
+# patch android version
+android_ver=$(file_getprop /system/build.prop ro.build.version.release);
+patch_cmdline androidboot.version androidboot.version=$android_ver
 write_boot;
 ## end boot install
 
@@ -46,7 +49,7 @@ write_boot;
 #block=vendor_boot;
 #is_slot_device=1;
 #ramdisk_compression=auto;
-#patch_vbmeta_flag=auto;
+patch_vbmeta_flag=auto;
 
 # reset for vendor_boot patching
 #reset_ak;
